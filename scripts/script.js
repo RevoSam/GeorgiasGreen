f9135c71b498be35bcc50b250dea78bd9658b6da
RevoSam <n-sami-m@hotmail.fr>
2021-11-28 14:33:35 -0500

    Cart bug fixed

--- scripts/script.js
+++ scripts/script.js
@@ -380,13 +380,17 @@ function deleteEntireRow(e) {
     var index = e.target.getAttribute("id");
     if (index > -1) {
       products.splice(index, 1);
+      document
+        .getElementById("cartTable")
+        .deleteRow(e.target.parentNode.parentNode.rowIndex);
+      updateSummary();
+      var anchors = document.getElementsByClassName("cart-item-delete");
+      for (index = 0; index < products.length; index++) {
+        anchors[index].setAttribute("id", index);
+      }
       var jsonString = JSON.stringify(products);
       window.localStorage.setItem("products", jsonString);
     }
-    document
-      .getElementById("cartTable")
-      .deleteRow(e.target.parentNode.parentNode.rowIndex);
-    updateSummary();
   } catch (error) {
     console.log(error);
   }
@@ -419,160 +423,3 @@ function addListener2QtyBox() {
     QtyText.addEventListener("keyup", change_updateProductExtendedTtl);
   }
 }
-
-/*TEST FOR BOX UPDATE TO DELETE IF CAUSE PROBLEMS*/
-
-/** End Script for Cart Pages */
-
-/*Script for product pages*/
-/*var plus_minus_button = document.getElementsByClassName("plus-minus-button");
-
-function updateQuantity() {
-  var int_data = parseInt(document.getElementById("quantity-text").value, 10);
-
-  if (this.value == "+")
-    if (document.getElementById("quantity-text").value == "")
-      document.getElementById("quantity-text").value = 1;
-    else document.getElementById("quantity-text").value = int_data + 1;
-  else {
-    if (!checkOOB(document.getElementById("quantity-text").value - 1))
-      document.getElementById("quantity-text").value = int_data - 1;
-  }
-  updateExtendedTtl();
-}
-
-function checkOOB(value) {
-  if (value < 0) return true;
-}
-
-function updateExtendedTtl() {
-  document.getElementById("total").value =
-    (
-      parseFloat(document.getElementById("price").innerText) *
-      document.getElementById("quantity-text").value
-    ).toFixed(2) + "$";
-}
-
-//EventListener
-for (var i = 0; i < plus_minus_button.length; i++) {
-  plus_minus_button[i].addEventListener("click", updateQuantity);
-}
-
-//Make it change on text input
-function change_updateProductExtendedTtl() {
-  if (isNaN(this.value)) this.value = 0;
-  updateExtendedTtl();
-}
-
-document
-  .getElementById("quantity-text")
-  .addEventListener("keyup", change_updateProductExtendedTtl);
-
-//Function for the Add more
-
-/*let showMoreBtn = document.getElementsByClassName("show-more");
-
-function showMoreDescription(e) {
-  //want the function to give show less
-  if (this.innerText == "Show More") {
-    this.innerText = "Show Less";
-    document.getElementById("content-description").style.display = "block";
-    document.getElementById("content-description").style.height = "150px";
-  } else {
-    this.innerText = "Show More";
-    document.getElementById("content-description").style.display = "none";
-  }
-}
-
-showMoreBtn[0].addEventListener("click", showMoreDescription);*/
-/* End Script for product pages*/
-
-/*
-function getCartTotal(myString, sign) {
-  console.log(myString);
-  var new_subtotal = parseFloat(document.getElementById("subtotal").innerHTML);
-
-  if (sign == "+")
-    new_subtotal =
-      new_subtotal +
-      parseFloat(document.getElementById(myString + "_price").innerHTML);
-  else
-    new_subtotal =
-      new_subtotal -
-      parseFloat(document.getElementById(myString + "_price").innerHTML);
-
-  var GST = new_subtotal * 0.05;
-  var QST = new_subtotal * 0.09975;
-
-  document.getElementById("subtotal").innerHTML =
-    parseFloat(new_subtotal).toFixed(2);
-  document.getElementById("gst_total").innerHTML = parseFloat(GST).toFixed(2);
-  document.getElementById("qst_total").innerHTML = parseFloat(QST).toFixed(2);
-  document.getElementById("total").innerHTML = parseFloat(
-    parseFloat(document.getElementById("subtotal").innerHTML) +
-      parseFloat(document.getElementById("gst_total").innerHTML) +
-      parseFloat(document.getElementById("qst_total").innerHTML)
-  ).toFixed(2);
-}
-*/
-
-/*End of update price using cart buttons*/
-
-/*function changeCartQty(e) {
-  var myString;
-  var anchor = e.target;
-  if (e.target.getAttribute("id") != null) {
-    myString = e.target.getAttribute("id");
-  } else {
-    myString = this.className.split(" ")[1].match(/item\d+/);
-  }
-  var int_data = parseInt(document.getElementById(myString).value, 10);
-
-  if (anchor.value == "+") {
-    if (document.getElementById(myString).value == "")
-      document.getElementById(myString).value = 1;
-    else document.getElementById(myString).value = int_data + 1;
-    getCartTotal(myString, this.value);
-  } else {
-    if (document.getElementById(myString).value == "")
-      document.getElementById(myString).value = 0;
-    else if (!checkOOB(int_data - 1)) {
-      document.getElementById(myString).value = int_data - 1;
-      getCartTotal(myString, this.value);
-
-    }
-  }
-}*/
-
-/*
-for (var i = 0; i < cartButtons.length; i++) {
-  cartButtons[i].addEventListener("click", changeCartQty);
-}
-*/
-
-/*
-for (var i = 0; i < inputBoxes.length; i++) {
-  inputBoxes[i].addEventListener("keyup", changeCartBox);
-}
-*/
-//end of price update using input box
-//Delete all stored quantities and values of this page from sessionStorage
-/*window.onbeforeunload = function(){
-  //deleteStorage();
-}
-
-function deleteStorage()
-{
-/*for (var quantityID in sessionStorage) {
-    if (quantityID.indexOf(currentPageID) === 0)
-      sessionStorage.removeItem(quantityID);
-  }
-  if (performance.navigation.type == performance.navigation.TYPE_RELOAD)
-  window.alert('reaches deleteStorage');
-  sessionStorage.removeItem(currentPageID+'quantity');
-  
-}*/
-
-/*function deleteProducts() {
-  window.localStorage.removeItem("products");
-}*/
