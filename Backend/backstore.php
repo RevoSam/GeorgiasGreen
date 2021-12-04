3646ec13a973b4178815efe17fc0565358efa783
akleidotos <akleidotos@protonmail.com>
2021-12-04 06:46:06 -0500

    added code for dairy and cleaning products

--- Backend/backstore.php
+++ Backend/backstore.php
@@ -1,3 +1,19 @@
+<?php 
+  $file_open_aisles = '../data/aisles.xml';//'../../data/aisles.xml'
+  $file_open_products = '../data/product.xml';
+  
+  if (file_exists($file_open_aisles) && file_exists($file_open_products))
+  {
+    $aisles = simplexml_load_file($file_open_aisles);
+    $products = simplexml_load_file($file_open_products);
+    $aisle_found = $aisles->xpath('/aisles/aisle/al_id[.= 5]/parent::*');
+    $dairy_products_found = $products->xpath('/products/product/pdt_al_id[.= 5]/parent::*');
+    $cleaning_products_found = $products->xpath('/products/product/pdt_al_id[.= 6]/parent::*');
+  }
+  else{
+    exit('Failed to open ');
+  }
+?>
 <!DOCTYPE html>
 <html>
 
@@ -561,269 +577,77 @@
             <div class="ailes">
                 <p>Dairy</p>
             </div>
-            <div class="product">
-                <div class="brand-container">
-                    <p>BRAND</p>
-                </div>
-                <div class="name-container">
-                    <p>NAME</p>
-                </div>
-                <div class="department-container">
-                    <p>DEPARTMENT</p>
-                </div>
-                <div class="price-container">
-                    <p>PRICE</p>
-                </div>
-                <div class="small-container">
-                    <p>QTY</p>
-                </div>
-                <div class="origin-container">
-                    <p>ORIGIN</p>
-                </div>
-                <div class="small-container">
-                    <button class = "edit-button" onclick="location.href='product-edit.php'"><p>EDIT</p></button>
-                </div>
-                <div class="small-container">
-                    <button class = "delete-button"><p>DELETE</p></button>
-                </div>
-            </div>
-            <div class="product">
-                <div class="brand-container">
-                    <p>BRAND</p>
-                </div>
-                <div class="name-container">
-                    <p>NAME</p>
-                </div>
-                <div class="department-container">
-                    <p>DEPARTMENT</p>
-                </div>
-                <div class="price-container">
-                    <p>PRICE</p>
-                </div>
-                <div class="small-container">
-                    <p>QTY</p>
-                </div>
-                <div class="origin-container">
-                    <p>ORIGIN</p>
-                </div>
-                <div class="small-container">
-                    <button class = "edit-button" onclick="location.href='product-edit.php'"><p>EDIT</p></button>
-                </div>
-                <div class="small-container">
-                    <button class = "delete-button"><p>DELETE</p></button>
-                </div>
-            </div>
-            <div class="product">
-                <div class="brand-container">
-                    <p>BRAND</p>
-                </div>
-                <div class="name-container">
-                    <p>NAME</p>
-                </div>
-                <div class="department-container">
-                    <p>DEPARTMENT</p>
-                </div>
-                <div class="price-container">
-                    <p>PRICE</p>
-                </div>
-                <div class="small-container">
-                    <p>QTY</p>
-                </div>
-                <div class="origin-container">
-                    <p>ORIGIN</p>
-                </div>
-                <div class="small-container">
-                    <button class = "edit-button" onclick="location.href='product-edit.php'"><p>EDIT</p></button>
-                </div>
-                <div class="small-container">
-                    <button class = "delete-button"><p>DELETE</p></button>
-                </div>
-            </div>
-            <div class="product">
-                <div class="brand-container">
-                    <p>BRAND</p>
-                </div>
-                <div class="name-container">
-                    <p>NAME</p>
-                </div>
-                <div class="department-container">
-                    <p>DEPARTMENT</p>
-                </div>
-                <div class="price-container">
-                    <p>PRICE</p>
-                </div>
-                <div class="small-container">
-                    <p>QTY</p>
-                </div>
-                <div class="origin-container">
-                    <p>ORIGIN</p>
-                </div>
-                <div class="small-container">
-                    <button class = "edit-button" onclick="location.href='product-edit.php'"><p>EDIT</p></button>
-                </div>
-                <div class="small-container">
-                    <button class = "delete-button"><p>DELETE</p></button>
-                </div>
-            </div>
-            <div class="product">
-                <div class="brand-container">
-                    <p>BRAND</p>
-                </div>
-                <div class="name-container">
-                    <p>NAME</p>
-                </div>
-                <div class="department-container">
-                    <p>DEPARTMENT</p>
-                </div>
-                <div class="price-container">
-                    <p>PRICE</p>
-                </div>
-                <div class="small-container">
-                    <p>QTY</p>
-                </div>
-                <div class="origin-container">
-                    <p>ORIGIN</p>
-                </div>
-                <div class="small-container">
-                    <button class = "edit-button" onclick="location.href='product-edit.php'"><p>EDIT</p></button>
-                </div>
-                <div class="small-container">
-                    <button class = "delete-button"><p>DELETE</p></button>
-                </div>
-            </div>
+            <?php 
+              foreach($dairy_products_found  as $item){
+                $id = urlencode($item->pdt_id);
+                $url = htmlspecialchars("product-edit.php?ID=". $id );
+                echo "<div class = \"product\">";
+                    echo "<div class = \"brand-container\">";
+                        echo "<p> {$item->pdt_brand_name}";
+                    echo "</div>";
+                    echo "<div class=\"name-container\">";
+                        echo "<p>{$item->pdt_name}</p>";
+                    echo "</div>";
+                    echo "<div class=\"department-container\">";
+                        echo "<p>DAIRY</p>";
+                    echo "</div>";
+                    echo "<div class=\"price-container\">";
+                        echo "<p>{$item->pdt_price}$</p>";
+                    echo "</div>";
+                    echo "<div class=\"small-container\">";
+                        echo "<p>{$item->pdt_inventory}</p>";
+                    echo "</div>";
+                    echo "<div class=\"origin-container\">";
+                        echo "<p>{$item->pdt_origin}</p>";
+                    echo "</div>";
+                    echo "<div class=\"small-container\">";
+                        echo "<button id = \"{$item->pdt_id}\" class = \"edit-button\" onclick=\"location.href='{$url}'\"><p>EDIT</p></button>";
+                    echo "</div>";
+                    echo "<div class=\"small-container\">";
+                        echo "<button class = \"delete-button\"><p>DELETE</p></button>";
+                    echo "</div>";
+                echo "</div>";
+              }
+          ?>
+          <!--
+                $id = urlencode($item->pdt_id);
+                $url = htmlspecialchars("../../Products/product.php?ID=". $id );
+            -->            
             <div class="ailes">
                 <p>Cleaning Supplies</p>
             </div>
-            <div class="product">
-                <div class="brand-container">
-                    <p>BRAND</p>
-                </div>
-                <div class="name-container">
-                    <p>NAME</p>
-                </div>
-                <div class="department-container">
-                    <p>DEPARTMENT</p>
-                </div>
-                <div class="price-container">
-                    <p>PRICE</p>
-                </div>
-                <div class="small-container">
-                    <p>QTY</p>
-                </div>
-                <div class="origin-container">
-                    <p>ORIGIN</p>
-                </div>
-                <div class="small-container">
-                    <button class = "edit-button" onclick="location.href='product-edit.php'"><p>EDIT</p></button>
-                </div>
-                <div class="small-container">
-                    <button class = "delete-button"><p>DELETE</p></button>
-                </div>
-            </div>
-            <div class="product">
-                <div class="brand-container">
-                    <p>BRAND</p>
-                </div>
-                <div class="name-container">
-                    <p>NAME</p>
-                </div>
-                <div class="department-container">
-                    <p>DEPARTMENT</p>
-                </div>
-                <div class="price-container">
-                    <p>PRICE</p>
-                </div>
-                <div class="small-container">
-                    <p>QTY</p>
-                </div>
-                <div class="origin-container">
-                    <p>ORIGIN</p>
-                </div>
-                <div class="small-container">
-                    <button class = "edit-button" onclick="location.href='product-edit.php'"><p>EDIT</p></button>
-                </div>
-                <div class="small-container">
-                    <button class = "delete-button"><p>DELETE</p></button>
-                </div>
-            </div>
-            <div class="product">
-                <div class="brand-container">
-                    <p>BRAND</p>
-                </div>
-                <div class="name-container">
-                    <p>NAME</p>
-                </div>
-                <div class="department-container">
-                    <p>DEPARTMENT</p>
-                </div>
-                <div class="price-container">
-                    <p>PRICE</p>
-                </div>
-                <div class="small-container">
-                    <p>QTY</p>
-                </div>
-                <div class="origin-container">
-                    <p>ORIGIN</p>
-                </div>
-                <div class="small-container">
-                    <button class = "edit-button" onclick="location.href='product-edit.php'"><p>EDIT</p></button>
-                </div>
-                <div class="small-container">
-                    <button class = "delete-button"><p>DELETE</p></button>
-                </div>
-            </div>
-            <div class="product">
-                <div class="brand-container">
-                    <p>BRAND</p>
-                </div>
-                <div class="name-container">
-                    <p>NAME</p>
-                </div>
-                <div class="department-container">
-                    <p>DEPARTMENT</p>
-                </div>
-                <div class="price-container">
-                    <p>PRICE</p>
-                </div>
-                <div class="small-container">
-                    <p>QTY</p>
-                </div>
-                <div class="origin-container">
-                    <p>ORIGIN</p>
-                </div>
-                <div class="small-container">
-                    <button class = "edit-button" onclick="location.href='product-edit.php'"><p>EDIT</p></button>
-                </div>
-                <div class="small-container">
-                    <button class = "delete-button"><p>DELETE</p></button>
-                </div>
-            </div>
-            <div class="product">
-                <div class="brand-container">
-                    <p>BRAND</p>
-                </div>
-                <div class="name-container">
-                    <p>NAME</p>
-                </div>
-                <div class="department-container">
-                    <p>DEPARTMENT</p>
-                </div>
-                <div class="price-container">
-                    <p>PRICE</p>
-                </div>
-                <div class="small-container">
-                    <p>QTY</p>
-                </div>
-                <div class="origin-container">
-                    <p>ORIGIN</p>
-                </div>
-                <div class="small-container">
-                    <button class = "edit-button" onclick="location.href='product-edit.php'"><p>EDIT</p></button>
-                </div>
-                <div class="small-container">
-                    <button class = "delete-button"><p>DELETE</p></button>
-                </div>
-            </div>
+            <?php 
+              foreach($cleaning_products_found  as $item){
+                $id = urlencode($item->pdt_id);
+                $url = htmlspecialchars("product-edit.php?ID=". $id );
+                echo "<div class = \"product\">";
+                    echo "<div class = \"brand-container\">";
+                        echo "<p> {$item->pdt_brand_name}";
+                    echo "</div>";
+                    echo "<div class=\"name-container\">";
+                        echo "<p>{$item->pdt_name}</p>";
+                    echo "</div>";
+                    echo "<div class=\"department-container\">";
+                        echo "<p>CLEANING SUPPLIES</p>";
+                    echo "</div>";
+                    echo "<div class=\"price-container\">";
+                        echo "<p>{$item->pdt_price}$</p>";
+                    echo "</div>";
+                    echo "<div class=\"small-container\">";
+                        echo "<p>{$item->pdt_inventory}</p>";
+                    echo "</div>";
+                    echo "<div class=\"origin-container\">";
+                        echo "<p>{$item->pdt_origin}</p>";
+                    echo "</div>";
+                    echo "<div class=\"small-container\">";
+                        echo "<button id = \"{$item->pdt_id}\" class = \"edit-button\" onclick=\"location.href='{$url}'\"><p>EDIT</p></button>";
+                    echo "</div>";
+                    echo "<div class=\"small-container\">";
+                        echo "<button class = \"delete-button\"><p>DELETE</p></button>";
+                    echo "</div>";
+                echo "</div>";
+              }
+          ?>
         </div>
     </div>
     </div>
